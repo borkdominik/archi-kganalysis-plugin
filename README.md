0e4c2f38f95e5b84d1309affc2b9ebf9af2b6619
plglaser <plg741@gmail.com>
2021-09-29 14:56:23 +0200

    Created Archi Plugin project with a basic exporter

--- README.md
+++ README.md
@@ -9,9 +9,6 @@
 [Archi User Guide](https://www.archimatetool.com/downloads/Archi%20User%20Guide.pdf)  
 [Archi GitHub Repo](https://github.com/archimatetool/archi) ([Wiki](https://github.com/archimatetool/archi/wiki))
 
-Archi is developed based on the Eclipse Rich Client Platform (RCP).  
-➜ [RCP Page containing Ressources](http://wiki.eclipse.org/Rich_Client_Platform)
-
 Archi Concepts and Colors:
 <img src="https://www.archimatetool.com/wp-content/uploads/2018/11/ArchiMate-3.0-Notation-Overview-ArchiMate-standard-default-color-scheme.png">
 
@@ -23,3 +20,21 @@ Archi Concepts and Colors:
 ### Papers
 [Verification of ArchiMate Behavioral
 Elements by Model Checking](https://link.springer.com/content/pdf/10.1007/978-3-319-24369-6_11.pdf) (Plugin for Archi)
+
+### Eclipse RCP
+
+Archi is developed based on the Eclipse Rich Client Platform (RCP).  
+➜ [RCP Page containing Ressources](http://wiki.eclipse.org/Rich_Client_Platform)
+
+Eclipse application consists of individual software components, called *plug-ins* (e.g. new menu entries, toolbar entries). Eclipse RCP application typically uses the same base components as the Eclipse IDE and also add application-specific components on top. Core Components of the Eclipse Platform:
+- **OSGi**, specification for modular Java applications
+- **Equinox**, implementation of the OSGi specification, used by Eclipse 
+- **SWT**,  standard  user  interface  component  library  used  by Eclipse (+ JFace)
+- **Workbench**, provides framework for the application, responsible for all other UI components
+
+<img src="https://www.vogella.com/tutorials/EclipseRCP/img/architecture20.png" width="60%">
+</center>
+
+Important configuration files:
+- `MANIFEST.MF`, contains OSGi configuration information (API and dependencies)
+- `plugin.xml`, contains Eclipse specific extension mechanisms, *Extension-points* (interfaces  for plug-ins  to  contribute  functionality) and *Extensions* (functionality)
---
+++ org.kganalysis/src/com/kganalysis/MyExporter.java
@@ -0,0 +1,123 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
+package com.kganalysis;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+
+import com.archimatetool.editor.model.IModelExporter;
+import com.archimatetool.model.FolderType;
+import com.archimatetool.model.IArchimateConcept;
+import com.archimatetool.model.IArchimateModel;
+import com.archimatetool.model.IFolder;
+
+
+
+/**
+ * Example Exporter of Archimate model
+ * 
+ */
+public class MyExporter implements IModelExporter {
+    
+    String MY_EXTENSION = ".mex"; //$NON-NLS-1$
+    String MY_EXTENSION_WILDCARD = "*.mex"; //$NON-NLS-1$
+    
+    private OutputStreamWriter writer;
+    
+    public MyExporter() {
+    }
+
+    @Override
+    public void export(IArchimateModel model) throws IOException {
+        File file = askSaveFile();
+        if(file == null) {
+            return;
+        }
+        
+        writer = new OutputStreamWriter(new FileOutputStream(file));
+        
+        writeFolder(model.getFolder(FolderType.STRATEGY));
+        writeFolder(model.getFolder(FolderType.BUSINESS));
+        writeFolder(model.getFolder(FolderType.APPLICATION));
+        writeFolder(model.getFolder(FolderType.TECHNOLOGY));
+        writeFolder(model.getFolder(FolderType.MOTIVATION));
+        writeFolder(model.getFolder(FolderType.IMPLEMENTATION_MIGRATION));
+        writeFolder(model.getFolder(FolderType.OTHER));
+        writeFolder(model.getFolder(FolderType.RELATIONS));
+        
+        writer.close();
+    }
+    
+    private void writeFolder(IFolder folder) throws IOException {
+        List<EObject> list = new ArrayList<EObject>();
+        
+        getElements(folder, list);
+        
+        for(EObject eObject : list) {
+            if(eObject instanceof IArchimateConcept) {
+                IArchimateConcept concept = (IArchimateConcept)eObject;
+                String string = normalise(concept.eClass().getName()) +
+                        "," + normalise(concept.getName()) //$NON-NLS-1$
+                        + "," + normalise(concept.getDocumentation()); //$NON-NLS-1$
+                writer.write(string + "\n"); //$NON-NLS-1$
+            }
+        }
+    }
+    
+    private void getElements(IFolder folder, List<EObject> list) {
+        for(EObject object : folder.getElements()) {
+            list.add(object);
+        }
+        
+        for(IFolder f : folder.getFolders()) {
+            getElements(f, list);
+        }
+    }
+
+    private String normalise(String s) {
+        if(s == null) {
+            return ""; //$NON-NLS-1$
+        }
+        
+        s = s.replace("\r\n", " "); //$NON-NLS-1$ //$NON-NLS-2$
+        s = "\"" + s + "\""; //$NON-NLS-1$ //$NON-NLS-2$
+        
+        return s;
+    }
+
+    /**
+     * Ask user for file name to save to
+     */
+    private File askSaveFile() {
+        FileDialog dialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+        dialog.setText("Export Model"); //$NON-NLS-1$
+        dialog.setFilterExtensions(new String[] { MY_EXTENSION_WILDCARD, "*.*" } ); //$NON-NLS-1$
+
+        // Set to true for consistency on all OSs
+        dialog.setOverwrite(true);
+        
+        String path = dialog.open();
+        if(path == null) {
+            return null;
+        }
+        
+        // Only Windows adds the extension by default
+        if(dialog.getFilterIndex() == 0 && !path.endsWith(MY_EXTENSION)) {
+            path += MY_EXTENSION;
+        }
+        
+        return new File(path);
+    }
+}
